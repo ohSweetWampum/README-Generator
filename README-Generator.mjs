9d2797ea1d4ce2ec9dbf5b7b7efdcd272cd7b07d
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-20 08:25:51 -0500

    "got rid of the const inquirer = require and const fs = require for import from syntax. Also changes the js file to .mjs"

---
+++ README-Generator.mjs
@@ -0,0 +1,78 @@
+import inquirer from 'inquirer';
+import fs from 'fs/promises';
+// making the prompts
+const questions =[ {
+		type: 'input',
+		name: 'title',
+		message: 'What is the title of your application?',
+	}, {
+		type: 'input',
+		name: 'breif',
+		message: 'Enter a 1 sentence description of your application',
+	}, {
+		type: 'input',
+		name: 'website',
+		message: 'Enter link to deployed site',
+	}, {
+		type: 'input',
+		name: 'description',
+		message: 'Enter a description of your application',
+	},
+	//table of contents
+	{
+		type: 'input',
+		name: 'installation',
+		message: 'Provide any installation instructions',
+	}, {
+		type: 'input',
+		name: 'usage',
+		message: 'Provide description of how to use your application:',
+	}, {
+		type: 'input',
+		name: 'technology',
+		message: 'Enter links to the offical sites of the technologies you used',
+	}, {
+		type: 'input',
+		name: 'gifUrl',
+		message: 'Enter the URL of your demo GIF',
+	}, {
+		type: 'input',
+		name: 'codeSnippet',
+		message: 'Enter your code snippet',
+	}, {
+		type: 'input',
+		name: 'codeDescription',
+		message: 'Provide description of the code snippet',
+	}, {
+		type: 'input',
+		name: 'learning',
+		message: 'Provide any things you learned making this application',
+	}, {
+		type: 'input',
+		name: 'github',
+		message: 'Enter your GitHub profile link:',
+	}, {
+		type: 'input',
+		name: 'portfolio',
+		message: 'Enter your portfolio link',
+	}, {
+		type: 'input',
+		name: 'linkedin',
+		message: 'Enter your linkedin profile link',
+	}, {
+		type: 'input',
+		name: 'credits',
+		message: 'Credit resources that helped you, add their links',
+	}, {
+		type: 'list',
+		name: 'license',
+		message: 'Choose a license for your application:',
+		choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None'],
+	},
+];
+
+
+
+
+
+ 
